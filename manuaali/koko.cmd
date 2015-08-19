fd3c0b001b9c5c96b4f36567ab9696e4202a8300
Tiina Partanen <tyynetyyne@gmail.com>
2015-08-19 08:36:24 +0300

    Changes to support fin-doc

--- manuaali/koko.cmd
+++ manuaali/koko.cmd
@@ -1,1 +1,1 @@
-scribble --htmls ++main-xref-in --style manual-style-koko.css --dest generoitu --redirect-main http://docs.racket-lang.org koodausta_kouluun_manuaali.scrbl
+scribble --htmls ++main-xref-in --style manual-style-koko.css --dest generoitu koodausta_kouluun_manuaali.scrbl
