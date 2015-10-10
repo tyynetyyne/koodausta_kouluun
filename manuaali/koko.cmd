2a79ce7e9919c5c0ef1d989e54dd11629a9fdeb8
lemieux <berthier_lemieux@hotmail.com>
2015-10-10 11:45:38 +0300

    Fixed cross scribl link issues
    Fix

--- manuaali/koko.cmd
+++ manuaali/koko.cmd
@@ -1,1 +1,1 @@
-scribble --htmls ++main-xref-in --style manual-style-koko.css --dest generoitu koodausta_kouluun_manuaali.scrbl
+scribble --htmls ++main-xref-in --style manual-style-koko.css --info-out ../build/manuaali.sxref --dest ../build/generoitu manuaali.scrbl
--- tehtavat/koko.cmd
+++ tehtavat/koko.cmd
@@ -1,1 +1,1 @@
-scribble --htmls ++main-xref-in --style manual-style-koko.css --dest generoitu koodausta_kouluun_oppilaan_tehtavat.scrbl
+scribble --htmls ++main-xref-in --style manual-style-koko.css ++info-in ../build/manuaali.sxref --dest ../build/generoitu koodausta_kouluun_oppilaan_tehtavat.scrbl
